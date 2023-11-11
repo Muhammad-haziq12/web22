420dbb07ed22d4abd5052312763019293a670c13
Muhammad-Haziq12 <muhammad.haziq@student.vinci.be>
2023-11-11 19:58:32 +0100

    structured-hmi tutorial.

---
+++ tutorials/pizzeria/hmi/structured/src/index.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+import 'animate.css';
+
+// eslint-disable-next-line import/no-unresolved
+import HomePage from './Components/Pages/Homepage';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Navbar from './Components/Navbar/Navbar';
+
+
+Header();
+Navbar();
+HomePage();
+Footer();
